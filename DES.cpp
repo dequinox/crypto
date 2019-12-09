a19a7a0b636520beec2acecc47f501c6ecbd9135
dequinox <umarov.doro@gmail.com>
2019-12-09 14:04:42 +0300

    Add Step 1 Create 16 subkeys

--- DES.cpp
+++ DES.cpp
@@ -49,11 +49,48 @@ const std::string DES::hex2bin(char _c) const
 void DES::initialise_subkeys()
 {
     std::bitset<56> K;
-
     for (unsigned i = 0; i < 56; i++)
     {
         K[i] = key[63 - (PC_1[i] - 1)];
     }
 
-    std::cout << K << std::endl;
+    std::bitset<28> left, right;
+    std::bitset<56> compressedKey;
+
+    for (unsigned i = 0; i < 28; i++)
+    {
+        left[i] = K[i];
+        right[i] = K[i + 28];
+    }
+
+    for (unsigned round = 0; round < 16; round++)
+    {
+        left = left_shift(left, Rotations[round]);
+        right = left_shift(right, Rotations[round]);
+
+        for (unsigned i = 0; i < 28; i++)
+        {
+            compressedKey[i] = left[i];
+            compressedKey[i + 28] = right[i];
+        }
+    
+        for (unsigned i = 0; i < 48; i++)
+        {
+            subkeys[round][i] = compressedKey[PC_2[i] - 1];
+        }
+    }
+}
+
+std::bitset<28> DES::left_shift(std::bitset<28> _key, unsigned _index)
+{
+    std::bitset<28> temp = _key;
+    for (unsigned i = 0; i < 28; i++)
+    {
+        if (i + _index < 28)
+            temp[i] = _key[i + _index];
+        else
+            temp[i] = _key[i + _index - 28];
+    }
+
+    return temp;
 }
