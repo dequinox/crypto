225838086c8ede6a0eb0a084c119c5c2fd9d3338
dequinox <umarov.doro@gmail.com>
2019-12-13 14:58:44 +0300

    Create directoriies & Add Blowfish template

---
+++ BLOWFISH/BLOWFISH.cpp
@@ -0,0 +1,15 @@
+#include "BLOWFISH.h"
+
+BLOWFISH::BLOWFISH()
+{
+}
+
+void BLOWFISH::set_key(unsigned char key[])
+{
+    generate_keys(key);
+}
+
+void BLOWFISH::generate_keys(unsigned char key[])
+{
+
+}
